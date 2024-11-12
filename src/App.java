e3e37600d70f623778ae977c22e4fc72b0e137ce
CamilaParedesRo <camila.paredes@epn.edu.ec>
2024-11-11 20:54:51 -0500

    Agregar importaciones y métodos para manejar series, figuras, cadenas, arrays, loading y autómatas en la clase App

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import Poli_retos.SerieNumerica.SerieNumerica_01;
+import Poli_retos.SerieNumerica.SerieNumerica_03;
+import Poli_retos.SerieNumerica.SerieNumerica_06;
+import Poli_retos.SerieNumerica.SerieNumerica_08;
 import Poli_retos.SerieNumerica.Serie_02;
 import Poli_retos.SerieNumerica.Serie_04;
 import Poli_retos.SerieNumerica.Serie_07;
@@ -7,37 +11,133 @@ import Poli_retos.SerieDeCaracteres.SerieCara_02;
 import Poli_retos.SerieDeCaracteres.SerieCara_04;
 import Poli_retos.SerieDeCaracteres.SerieCara_07;
 import Poli_retos.SerieDeCaracteres.SerieCara_09;
+import Poli_retos.SerieDeCaracteres.SerieDeCaracter_03;
+import Poli_retos.SerieDeCaracteres.SerieDeCaracter_08;
+import Poli_retos.SerieDeCaracteres.SerieDeCaracteres_01;
+import Poli_retos.SerieDeCaracteres.SerieDeCaracteres_06;
+import Poli_retos.Loading.Loading_01;
+import Poli_retos.Loading.Loading_04;
+import Poli_retos.Loading.Loading_05;
+import Poli_retos.Loading.Loading_06;
 import Poli_retos.Loading.Loading_07;
+import Poli_retos.Loading.Loading_09;
+import Poli_retos.Loading.Loading_11;
 import Poli_retos.Loading.Loading_12;
+import Poli_retos.Recursion.Recursion_02;
+import Poli_retos.Recursion.Recursion_03;
+import Poli_retos.Recursion.Recursion_04;
 import Poli_retos.Recursion.Recursividad_05;
+import Poli_retos.Array.Array_02;
+import Poli_retos.Array.Array_04;
+import Poli_retos.Array.Array_05;
+import Poli_retos.Array.Arreglo_01;
+import Poli_retos.Automata.Automata_01;
+import Poli_retos.Automata.Automata_02;
+import Poli_retos.Automata.Automata_03;
+import Poli_retos.Automata.Automata_03_1;
+import Poli_retos.Automata.Automata_04;
+import Poli_retos.Automata.Automata_06;
+import Poli_retos.CadenaDeCaracteres.CadenaDeCaracter_04;
+import Poli_retos.CadenaDeCaracteres.CadenaDeCaracter_05;
+import Poli_retos.CadenaDeCaracteres.CadenaDeCaracteres_03;
+import Poli_retos.CadenaDeCaracteres.CadenaDeCaracteres_08;
 import Poli_retos.CadenaDeCaracteres.Cadena_01;
 import Poli_retos.CadenaDeCaracteres.Cadena_06;
+import Poli_retos.Figura.Figura_02;
+import Poli_retos.Figura.Figura_03;
+import Poli_retos.Figura.Figura_04;
+import Poli_retos.Figura.Figura_05;
+import Poli_retos.Figura.Figura_07;
+import Poli_retos.Figura.Figura_08;
+import Poli_retos.Figura.Figura_09;
+import Poli_retos.Figura.Figura_10;
+import Poli_retos.Figura.Figura_12;
+import Poli_retos.Figura.Figura_14;
+import Poli_retos.Figura.Figura_15;
+import Poli_retos.Figura.Figura_17;
+import Poli_retos.Figura.Figura_18;
+import Poli_retos.Figura.Figura_19;
 
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+       public static void main(String[] args) throws Exception {
+
         Scanner scanner = new Scanner(System.in);
-        
+        // Series Nuemricas 
+        SerieNumerica_01 serieNumerica = new SerieNumerica_01();
         Serie_02 serie2 = new Serie_02();
+        SerieNumerica_03 serieNumerica3 = new SerieNumerica_03();
         Serie_04 serie4 = new Serie_04();
+        SerieNumerica_06 serieNumerica6 = new SerieNumerica_06();
         Serie_07 serie7 = new Serie_07();
+        SerieNumerica_08 serieNumerica8 = new SerieNumerica_08();
         Serie_09 serie9 = new Serie_09();
         Serie_12 serie12 = new Serie_12();
-        
+
+        //Series de Caracteres
+        SerieDeCaracteres_01 serieDeCaracteres = new SerieDeCaracteres_01();
         SerieCara_02 serieCara2 = new SerieCara_02();
+        SerieDeCaracter_03 serieDeCaracter3 = new SerieDeCaracter_03();
         SerieCara_04 serieCara4 = new SerieCara_04();
+        SerieDeCaracteres_06 serieDeCaracteres6 = new SerieDeCaracteres_06();
         SerieCara_07 serieCara7 = new SerieCara_07();
+        SerieDeCaracter_08 serieDeCaracter8 = new SerieDeCaracter_08();
         SerieCara_09 serieCara9 = new SerieCara_09();
         
-        Loading_07 loading7 = new Loading_07();
-        Loading_12 loading12 = new Loading_12();
+        //Figuras
+        Figura_02 figura2 = new Figura_02();
+        Figura_03 figura3 = new Figura_03();
+        Figura_04 figura4 = new Figura_04();
+        Figura_05 figura5 = new Figura_05();
+        Figura_07 figura7 = new Figura_07();
+        Figura_08 figura8 = new Figura_08();
+        Figura_09 figura9 = new Figura_09();
+        Figura_10 figura10 = new Figura_10();
+        Figura_12 figura_12 = new Figura_12();
+        Figura_14 figura_14 = new Figura_14();
+        Figura_15 figura_15 = new Figura_15();
+        Figura_17 figura_17 = new Figura_17();
+        Figura_18 figura_18 = new Figura_18();
+        Figura_19 figura_19 = new Figura_19();
         
-        Recursividad_05 recursion = new Recursividad_05();
-        
+        //Cadena de caracteres
         Cadena_01 cadena1 = new Cadena_01();
         Cadena_06 cadena6 = new Cadena_06();
+        CadenaDeCaracteres_03 cadenaDeCaracteres_03 = new CadenaDeCaracteres_03();
+        CadenaDeCaracter_04 cadenaDeCaracter_04 = new CadenaDeCaracter_04();
+        CadenaDeCaracter_05 cadenaDeCaracter_05 = new CadenaDeCaracter_05();
+        CadenaDeCaracteres_08 cadenaDeCaracteres_08 = new CadenaDeCaracteres_08();
+
+        //Arrays
+        Arreglo_01 arreglo_01 = new Arreglo_01();
+        Array_02 array2 = new Array_02();
+        Array_04 array_04 = new Array_04();
+        Array_05 array_05 = new Array_05();
+
+        //Loading
+        Loading_01 loading1 = new Loading_01();
+        Loading_04 loading4 = new Loading_04();
+        Loading_05 loading5 = new Loading_05();
+        Loading_06 loading6 = new Loading_06();
+        Loading_07 loading7 = new Loading_07();
+        Loading_09 loading9 = new Loading_09();
+        Loading_11 loading11 = new Loading_11();
+        Loading_12 loading12 = new Loading_12();
         
+        //Recursividad
+        Recursion_02 recursion2 = new Recursion_02();
+        Recursion_03 recursion3 = new Recursion_03();
+        Recursion_04 recursion4 = new Recursion_04();
+        Recursividad_05 recursion = new Recursividad_05();
+        //Automatas
+        Automata_01 automata1 = new Automata_01();
+        Automata_02 automata2 = new Automata_02();
+        Automata_03 automata3 = new Automata_03();
+        Automata_03_1 automata3_1 = new Automata_03_1();
+        Automata_04 automata4 = new Automata_04();
+        Automata_06 automata6 = new Automata_06();
+       
         while (true) {
             System.out.println("\nSeleccione una opción:");
             System.out.println("1. Serie Numérica S2");
@@ -138,4 +238,29 @@ public class App {
         scanner.close();
         System.out.println("Programa finalizado.");
     }
+
+
+    public static int validarNumeros(){
+        Scanner scanner = new Scanner(System.in);
+             int extension;
+        while (true) {
+           System.out.println("Ingrese la extensión de las series: ");
+           if (scanner.hasNextInt()) {
+               extension = scanner.nextInt();
+               if (extension > 0) {
+                break; 
+            } else {
+                System.out.println("Error: Por favor, ingrese un número entero positivo.");
+            } 
+           } else {
+               System.out.println("Error: Por favor, ingrese un número entero válido.");
+               scanner.next(); 
+           }
+        }
+        scanner.close();
+         return extension;
+       } 
+
+
+
 }
